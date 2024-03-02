b50eed938f9f2927bb1f5a563bab04c8057cd145
Nazarí González <nazari.nz@gmail.com>
2024-03-02 21:18:20 +0000

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,16 @@ struct Entity {
 
 #[derive(AppState)]
 struct State {
-    rng: Random,
     entities: Vec<Entity>,
     pause: bool,
 }
 
 #[notan_main]
 fn main() -> Result<(), String> {
+    let win = WindowConfig::default().set_vsync(true);
+
     notan::init_with(setup)
+        .add_config(win)
         .add_config(DrawConfig)
         .update(update)
         .draw(draw)
@@ -39,32 +41,8 @@ fn main() -> Result<(), String> {
 }
 
 fn setup() -> State {
-    let mut rng = Random::default();
-    let entities = (0..INITIAL_ENTITIES)
-        .map(|_| {
-            let position = vec2(
-                50.0 + rng.gen::<f32>() * 700.0,
-                50.0 + rng.gen::<f32>() * 500.0,
-            );
-            let min = INITIAL_VELOCITY * -0.5;
-            let max = INITIAL_VELOCITY;
-            let velocity = vec2(min + rng.gen::<f32>() * max, min + rng.gen::<f32>() * max);
-            Entity {
-                body: Body {
-                    position,
-                    velocity,
-                    radius: 16.0,
-                },
-                transform: Transform {
-                    position,
-                    size: vec2(32.0, 32.0),
-                },
-                is_colliding: false,
-            }
-        })
-        .collect::<Vec<_>>();
+    let entities = init_entities();
     State {
-        rng,
         entities,
         pause: false,
     }
@@ -75,15 +53,15 @@ fn update(app: &mut App, state: &mut State) {
         state.pause = !state.pause;
     }
 
-    let delta = {
-        if state.pause {
-            0.0
-        } else {
-            app.timer.delta_f32()
-        }
-    };
+    if state.pause {
+        return;
+    }
+
+    // -- logic
+    let delta = app.timer.delta_f32();
 
     sys_clean_collisions(&mut state.entities);
+
     let collisions = sys_check_collision(&mut state.entities);
     sys_resolve_collisions(&mut state.entities, collisions);
     sys_bounce_rect(&mut state.entities);
@@ -91,7 +69,7 @@ fn update(app: &mut App, state: &mut State) {
     sys_body_to_transform(&mut state.entities);
 }
 
-fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
+fn draw(gfx: &mut Graphics, state: &mut State) {
     let mut draw = gfx.create_draw();
     draw.clear(Color::BLACK);
 
@@ -116,18 +94,46 @@ fn draw(app: &mut App, gfx: &mut Graphics, state: &mut State) {
     gfx.render(&draw);
 }
 
+fn init_entities() -> Vec<Entity> {
+    let mut rng = Random::default();
+    (0..INITIAL_ENTITIES)
+        .map(|_| {
+            let position = vec2(
+                50.0 + rng.gen::<f32>() * 700.0,
+                50.0 + rng.gen::<f32>() * 500.0,
+            );
+            let min = INITIAL_VELOCITY * -0.5;
+            let max = INITIAL_VELOCITY;
+            let velocity = vec2(min + rng.gen::<f32>() * max, min + rng.gen::<f32>() * max);
+            Entity {
+                body: Body {
+                    position,
+                    velocity,
+                    radius: 16.0,
+                },
+                transform: Transform {
+                    position,
+                    size: vec2(32.0, 32.0),
+                },
+                is_colliding: false,
+            }
+        })
+        .collect()
+}
+
 fn is_colliding(p1: Vec2, r1: f32, p2: Vec2, r2: f32) -> bool {
     let min_dist = r1 + r2;
     let diff = p1 - p2;
     diff.dot(diff).sqrt() <= min_dist
 }
 
+// systems
 fn sys_clean_collisions(entities: &mut [Entity]) {
     entities.iter_mut().for_each(|e| e.is_colliding = false);
 }
 
 fn sys_check_collision(entities: &mut [Entity]) -> Vec<(usize, usize)> {
-    // TODO do not next loops, use spatial hashing
+    // TODO do not nest loops, use spatial hashing
     let mut colliding = vec![];
     entities.iter().enumerate().for_each(|(id1, e1)| {
         entities.iter().enumerate().for_each(|(id2, e2)| {
@@ -164,8 +170,7 @@ fn sys_resolve_collisions(entities: &mut [Entity], collisions: Vec<(usize, usize
         let diff_vel = b1.velocity - b2.velocity;
         let speed = (diff_vel * normalized).length_squared();
         if speed < 0.0 || !speed.is_finite() {
-            println!("{speed}");
-            return;
+            panic!("Speed: {}", speed);
         }
 
         entities[id1].body.velocity -= speed * normalized;
@@ -175,11 +180,15 @@ fn sys_resolve_collisions(entities: &mut [Entity], collisions: Vec<(usize, usize
 
 fn sys_bounce_rect(entities: &mut [Entity]) {
     entities.iter_mut().for_each(|e| {
-        if e.body.position.x - e.body.radius < 0.0 || e.body.position.x + e.body.radius >= 800.0 {
+        let left = e.body.position.x - e.body.radius <= 0.0;
+        let right = e.body.position.x + e.body.radius >= 800.0;
+        if left && e.body.velocity.x < 0.0 || right && e.body.velocity.x > 0.0 {
             e.body.velocity.x *= -1.0;
         }
 
-        if e.body.position.y - e.body.radius < 0.0 || e.body.position.y + e.body.radius >= 600.0 {
+        let top = e.body.position.y - e.body.radius < 0.0;
+        let bottom = e.body.position.y + e.body.radius >= 600.0;
+        if top && e.body.velocity.y < 0.0 || bottom && e.body.velocity.y > 0.0 {
             e.body.velocity.y *= -1.0;
         }
     });
